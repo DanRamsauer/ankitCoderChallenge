0b079be241a4fdb017f465e8768285e751aee5b7
DanielRamsauer <danramsauer@gmail.com>
2023-02-24 15:27:14 -0500

    styled button

--- scripts.js
+++ scripts.js
@@ -1,12 +1,23 @@
+const ankitChallenge = {};
+
+ankitChallenge.init = () => {
+    // call functions in here
+    ankitChallenge.faqAccordion();
+}
+
 const faqs = document.querySelectorAll(".questionsDiv");
 const answer = document.querySelectorAll(".answer");
 
-// answer.hidden = true;
 
-faqs.forEach( (faq) => {
-        // adding event listener to each faq div
-        faq.addEventListener("click", (e) => {
-        // when its clicked on toggle the class active on the div
-        faq.classList.toggle("active");
+ankitChallenge.faqAccordion = () => {
+    faqs.forEach( (faq) => {
+            // adding event listener to each faq div
+            faq.addEventListener("click", (e) => {
+            // when its clicked on toggle the class active on the div
+            faq.classList.toggle("active");
+        });
     });
-});
+}
+
+
+ankitChallenge.init();
