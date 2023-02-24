2189340d25020f9db02d1b592b898adb935f7b31
DanielRamsauer <danramsauer@gmail.com>
2023-02-24 18:57:05 -0500

    finished styling and functionality of slideout nav

--- scripts.js
+++ scripts.js
@@ -3,22 +3,51 @@ const ankitChallenge = {};
 ankitChallenge.init = () => {
     // call functions in here
     ankitChallenge.faqAccordion();
+    ankitChallenge.closeSlideOutNav();
+    ankitChallenge.openSlideOutNav();
+    ankitChallenge.sidebarAnchor();
 }
 
 const faqs = document.querySelectorAll(".questionsDiv");
 const answer = document.querySelectorAll(".answer");
-const hamburgerMenu = document.querySelectorAll(".hamburgerMenu");
+const sidebarAnchor = document.querySelectorAll(".sidebarA");
+const X = document.querySelector("#closeSlide");
+const slideNav = document.querySelector("#slidebar");
+const hamburgerMenu = document.querySelector("#hamburgerMenu");
 
 
 ankitChallenge.faqAccordion = () => {
     faqs.forEach( (faq) => {
             // adding event listener to each faq div
-            faq.addEventListener("click", (e) => {
+            faq.addEventListener("click", () => {
             // when its clicked on toggle the class active on the div
             faq.classList.toggle("active");
         });
     });
-}
+};
+
+ankitChallenge.sidebarAnchor = () => {
+    sidebarAnchor.forEach( (anchor) => {
+            // adding event listener to each faq div
+            anchor.addEventListener("click", () => {
+            // when its clicked on remove the class to get rid of the sidebarnav
+            slideNav.classList.remove("open");
+        });
+    });
+};
+
+ankitChallenge.closeSlideOutNav = () => {
+    X.addEventListener("click", () => {
+        slideNav.classList.remove("open");
+    });
+};
+
+ankitChallenge.openSlideOutNav = () => {
+    hamburgerMenu.addEventListener("click", () => {
+        slideNav.classList.toggle("open");
+        setTimeout()
+    });
+};
 
 
 ankitChallenge.init();
